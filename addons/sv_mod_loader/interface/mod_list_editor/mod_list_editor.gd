9c3f8bf053b7652b129865aa54dd3f2f7ff5ce82
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2024-06-07 11:17:21 +0100

    Add exported path properties

--- addons/sv_mod_loader/interface/mod_list_editor/mod_list_editor.gd
+++ addons/sv_mod_loader/interface/mod_list_editor/mod_list_editor.gd
@@ -4,3 +4,7 @@ extends VBoxContainer
 ## UI Element where you can choose and edit a mod list. You can also delete
 ## mod lists and create new ones.
 
+## File path where mod lists are stored.
+@export var mod_list_path: String = "user://mod_lists"
+## File path where mods are stored.
+@export var mod_path: String = "user://mods"
--- addons/sv_mod_loader/interface/mod_loader/mod_loader.gd
+++ addons/sv_mod_loader/interface/mod_loader/mod_loader.gd
@@ -4,3 +4,19 @@ extends Control
 ## This scene contains a full interface that allows setup of a mod load order
 ## and starting the game with that load order.
 
+## Path of directory where mod lists are stored. Used to overwrite the same
+## property configured on mod_list_editor.
+@export var mod_list_path: String = "user://mod_lists"
+## Path of directory where mods are stored. Used to overwrite the same property
+## configured on mod_list_editor.
+@export var mod_path: String = "user://mods"
+@export_group("Internal Nodes")
+## Child mod list editor scene
+@export var mod_list_editor: Node
+
+
+func _on_mod_list_editor_tree_entered():
+	# Using tree_entered signal because this needs to be done before ready,
+	# which is when mod_list_editor uses these values
+	mod_list_editor.mod_list_path = mod_list_path
+	mod_list_editor.mod_path = mod_path
