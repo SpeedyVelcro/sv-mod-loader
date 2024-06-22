a81e8c7187cdaad5a4ede1a34148518ce6b85225
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2024-06-22 19:35:29 +0100

    Helper functions for saving and loading mod lists

--- addons/sv_mod_loader/interface/mod_array_editor/mod_array_editor.gd
+++ addons/sv_mod_loader/interface/mod_array_editor/mod_array_editor.gd
@@ -78,6 +78,24 @@ func set_mod_array(mod_array: Array[Mod]) -> void:
 	for mod in mod_array:
 		_append_mod(mod)
 
+
+## Updates the current mod array to match the given mod array. Does not add new
+## mods. Disables mods that aren't present in the given mod array. Reorders mods
+## to match the given mod array.
+func update_mod_array(mod_array: Array[Mod]) -> void:
+	for tree_item in get_root().get_children():
+		tree_item.set_checked(0, false)
+	
+	for i in mod_array.size():
+		var mod = mod_array[i]
+		var tree_item = _find_tree_item(mod.filename)
+		
+		if tree_item == null:
+			continue
+		
+		tree_item.set_checked(0, mod.enabled)
+		_move(tree_item, i)
+
 ## Adds a mod to the end of the list. Returns the resulting TreeItem
 func _append_mod(mod: Mod) -> TreeItem:
 	var item = create_item(get_root())
@@ -97,3 +115,31 @@ func _get_mod(tree_item: TreeItem) -> Mod:
 	mod.enabled = tree_item.is_checked(0)
 		
 	return mod
+
+
+## Finds the TreItem corresponding to the given mod name. Returns null if the
+## search fails
+func _find_tree_item(mod_name: String) -> TreeItem:
+	for tree_item in get_root().get_children():
+		if mod_name == tree_item.get_text(1):
+			return tree_item
+	return null
+
+
+## Moves the given tree item to the given position. Position must be
+## positive, and is indexed from 0. If the position is impossibly high, mod is
+## placed at the end.
+func _move(tree_item: TreeItem, pos: int) -> void:
+	if tree_item == null:
+		return
+	
+	var last_index = get_root().get_child_count() - 1
+	
+	if pos < 0:
+		return
+	
+	if pos >= last_index:
+		tree_item.move_after(get_root().get_child(-1))
+		return
+	
+	tree_item.move_before(get_root().get_child(pos + 1))
--- addons/sv_mod_loader/interface/mod_list_editor/mod_list_editor.gd
+++ addons/sv_mod_loader/interface/mod_list_editor/mod_list_editor.gd
@@ -13,10 +13,31 @@ extends VBoxContainer
 ## Child load order editor
 @export var mod_array_editor: Node
 
+
+## Name of the currently selected mod list
+var mod_list_name: String = ""
+
 # Override
 func _ready():
 	var mod_array: Array[Mod] = _get_mods_in_mod_path()
 	mod_array_editor.set_mod_array(mod_array)
+
+
+## Save the currently selected mod list
+func _save_current() -> void:
+	pass # TODO
+
+
+## Load the currently selected mod list
+func _load_current() -> void:
+	pass # TODO
+
+
+## Select and load the mod list with given name
+func _select(new_name: String) -> void:
+	_save_current()
+	mod_list_name = new_name
+	_load_current()
 
 
 ## Gets an array of all mods on disk stored in the mod_path
@@ -40,10 +61,55 @@ func _get_mods_in_mod_path() -> Array[Mod]:
 
 
 ## Returns true if the given filename is a mod by checking the extension
-func _is_filename_mod(filename: String):
+func _is_filename_mod(filename: String) -> bool:
 	var regex = RegEx.new()
 	regex.compile("\\.[pP][cC][kK]$")
 	
 	var results = regex.search_all(filename)
 	
 	return results.size() > 0
+
+
+## Gets the current configuration as a ModList
+func get_mod_list() -> ModList:
+	var mod_array: Array[Mod] = mod_array_editor.get_mod_array()
+	var mod_list: ModList = ModList.new()
+	
+	mod_list.name = _path_to_name(mod_list_path)
+	mod_list.load_order = mod_array_editor.get_mod_array()
+	
+	return mod_list
+
+
+## Configures according to given ModList
+func _set_mod_list(mod_list: ModList) -> void:
+	mod_list_name = _name_to_path(mod_list.name)
+	mod_array_editor.update_mod_array(mod_list.load_order)
+
+
+## Converts a mod list file path to the name of the mod list
+func _path_to_name(path: String) -> String:
+	path = path.trim_prefix(mod_list_path)
+	path = path.trim_suffix(".mli")
+	
+	return path
+
+
+## Converts a name of a mod list to the corresponding file path
+func _name_to_path(name_to_convert: String) -> String:
+	return mod_list_path + "/" + name_to_convert + ".mli"
+
+
+# Signal connection
+func _on_new_button_pressed() -> void:
+	pass # TODO
+
+
+# Signal connection
+func _on_delete_button_pressed() -> void:
+	pass # TODO
+
+
+# Signal connection
+func _on_option_button_item_selected(index) -> void:
+	pass # TODO
