f59e9591990516d3f04441cedc9d33fc3fd0a1d8
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2025-08-22 23:46:19 +0100

    Update mod array editor to handle mod requirements

--- addons/sv_mod_loader/interface/mod_array_editor/mod_array_editor.gd
+++ addons/sv_mod_loader/interface/mod_array_editor/mod_array_editor.gd
@@ -2,14 +2,77 @@ extends Tree
 ## UI Element for editing an array of mods
 ##
 ## This UI element displays an array of mods, and allows re-ordering and enabling
-## or disabling them.
+## or disabling them. A purely cosmetic list of mod requirements can be
+## displayed at the beginning of the list. These mod requirements are
+## un-editable and don't have any really useful data associated with them
+## (because the consumer of this node should already know that data)
+
+enum Column {CHECK = 0, NAME = 1}
+
+
+## Returns the load order as an array
+func get_mod_array() -> Array[Mod]:
+	var mod_array: Array[Mod] = []
+	
+	for tree_item in get_root().get_children():
+		if _is_required(tree_item):
+			continue
+		
+		var mod = _get_mod(tree_item)
+		mod_array.append(mod)
+	
+	return mod_array
+
+
+## Set an array of mods to be displayed and edited
+func set_mod_array(mod_array: Array[Mod]) -> void:
+	_clear_mods()
+	
+	for mod in mod_array:
+		_append_mod(mod)
+
+
+## Sets mod requirements to be displayed - uneditable - at the beginning of the
+## list
+func set_mod_requirements(mod_requirements: Array[ModRequirement]) -> void:
+	_clear_requirements()
+	
+	for mod_requirement in mod_requirements:
+		_append_mod_requirement(mod_requirement)
+
+## Updates the current mod array to match the given mod array. Does not add new
+## mods. Disables mods that aren't present in the given mod array. Reorders mods
+## to match the given mod array.
+func update_mod_array(mod_array: Array[Mod]) -> void:
+	for tree_item in get_root().get_children():
+		if not _is_required(tree_item):
+			tree_item.set_checked(Column.CHECK, false)
+	
+	var first_index_after_requirements = _get_last_mod_requirement().get_index() + 1
+	
+	for i in mod_array.size():
+		var mod = mod_array[i]
+		var tree_item = _find_tree_item(mod.filename)
+		
+		if tree_item == null:
+			continue
+		
+		if (_is_required(tree_item)):
+			continue
+		
+		tree_item.set_checked(Column.CHECK, mod.enabled)
+		_move(tree_item, first_index_after_requirements + i)
+
+
+# Override
+func _init() -> void:
+	create_item() # Root
 
 
 # Override
 func _ready() -> void:
-	# TODO: move magic numbers to enums e.g. COLUMN_CHECKBOX, COLUMN_FILENAME
-	set_column_expand(0, false) # Checkbox
-	set_column_expand(1, true) # File name
+	set_column_expand(Column.CHECK, false) # Checkbox
+	set_column_expand(Column.NAME, true) # File name
 
 
 # Override
@@ -30,6 +93,10 @@ func _drop_data(at_position: Vector2, data) -> void:
 	if data.get_tree() != self:
 		return
 	
+	# Mod requirements cannot be re-ordered
+	if _is_required(data):
+		return
+	
 	var target_item: TreeItem = get_item_at_position(at_position)
 	
 	if target_item == null:
@@ -45,6 +112,10 @@ func _drop_data(at_position: Vector2, data) -> void:
 func _get_drag_data(at_position: Vector2):
 	var data: TreeItem = get_item_at_position(at_position)
 	
+	# Mod requirements cannot be re-ordered
+	if _is_required(data):
+		data = null
+	
 	if data != null:
 		set_drag_preview(_create_drag_preview(data))
 	
@@ -59,60 +130,60 @@ func _create_drag_preview(tree_item: TreeItem) -> Control:
 	
 	return label
 
-## Returns the load order as an array
-func get_mod_array() -> Array[Mod]:
-	var mod_array: Array[Mod] = []
-	
-	for tree_item in get_root().get_children():
-		var mod = _get_mod(tree_item)
-		mod_array.append(mod)
-	
-	return mod_array
-
-
-## Set an array of mods to be displayed and edited
-func set_mod_array(mod_array: Array[Mod]) -> void:
-	clear()
-	create_item() # Root
-	
-	for mod in mod_array:
-		_append_mod(mod)
-
-
-## Updates the current mod array to match the given mod array. Does not add new
-## mods. Disables mods that aren't present in the given mod array. Reorders mods
-## to match the given mod array.
-func update_mod_array(mod_array: Array[Mod]) -> void:
-	for tree_item in get_root().get_children():
-		tree_item.set_checked(0, false)
-	
-	for i in mod_array.size():
-		var mod = mod_array[i]
-		var tree_item = _find_tree_item(mod.filename)
-		
-		if tree_item == null:
-			continue
-		
-		tree_item.set_checked(0, mod.enabled)
-		_move(tree_item, i)
 
 ## Adds a mod to the end of the list. Returns the resulting TreeItem
 func _append_mod(mod: Mod) -> TreeItem:
 	var item = create_item(get_root())
 	
-	item.set_cell_mode(0, TreeItem.CELL_MODE_CHECK)
-	item.set_editable(0, true)
-	item.set_checked(0, mod.enabled)
-	item.set_text(1, mod.filename)
+	item.set_cell_mode(Column.CHECK, TreeItem.CELL_MODE_CHECK)
+	item.set_editable(Column.CHECK, true)
+	item.set_checked(Column.CHECK, mod.enabled)
+	item.set_text(Column.NAME, mod.filename)
 	
 	return item
 
+
+## Appends the given mod requirement to the list of requirements at the
+## beginning of the array. Returns the resulting TreeItem
+func _append_mod_requirement(mod_requirement: ModRequirement) -> TreeItem:
+	var item = create_item(get_root())
+	
+	var last = _get_last_mod_requirement()
+	if last != null:
+		item.move_after(_get_last_mod_requirement())
+	else:
+		_move(item, 0)
+	
+	item.set_cell_mode(Column.CHECK, TreeItem.CELL_MODE_CHECK)
+	item.set_editable(Column.CHECK, false)
+	item.set_checked(Column.CHECK, true)
+	item.set_text(Column.NAME, mod_requirement.display_name)
+	
+	return item
+
+
+## Gets the tree item representing the last mod requirement in the mod list.
+## Returns null if there are no mod requirements.
+func _get_last_mod_requirement() -> TreeItem:
+	var tree_items = get_root().get_children()
+	var index = tree_items.rfind_custom(_is_required.bind())
+	
+	if index == -1:
+		return null
+	
+	else:
+		return tree_items[index]
+
+
 ## Gets the mod represented by the specified TreeItem
 func _get_mod(tree_item: TreeItem) -> Mod:
+	if (_is_required(tree_item)):
+		push_error("Attempted to get mod from a tree item which was actually a mod requirement: " + tree_item.get_text(Column.NAME))
+	
 	var mod = Mod.new()
 	
-	mod.filename = tree_item.get_text(1)
-	mod.enabled = tree_item.is_checked(0)
+	mod.filename = tree_item.get_text(Column.NAME)
+	mod.enabled = tree_item.is_checked(Column.CHECK)
 		
 	return mod
 
@@ -121,7 +192,7 @@ func _get_mod(tree_item: TreeItem) -> Mod:
 ## search fails
 func _find_tree_item(mod_name: String) -> TreeItem:
 	for tree_item in get_root().get_children():
-		if mod_name == tree_item.get_text(1):
+		if mod_name == tree_item.get_text(Column.NAME):
 			return tree_item
 	return null
 
@@ -143,3 +214,22 @@ func _move(tree_item: TreeItem, pos: int) -> void:
 		return
 	
 	tree_item.move_before(get_root().get_child(pos + 1))
+
+
+## Removes all required mods from the tree
+func _clear_requirements() -> void:
+	for tree_item: TreeItem in get_root().get_children():
+		if (_is_required(tree_item)):
+			get_root().remove_child(tree_item)
+
+
+## Removes all non-required mods from the tree
+func _clear_mods() -> void:
+	for tree_item: TreeItem in get_root().get_children():
+		if (not _is_required(tree_item)):
+			get_root().remove_child(tree_item)
+
+
+## True if the given tree item represents a required mod
+func _is_required(tree_item: TreeItem):
+	return not tree_item.is_editable(Column.CHECK)
