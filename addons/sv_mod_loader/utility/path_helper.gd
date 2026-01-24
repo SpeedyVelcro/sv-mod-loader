38f5cbf81cb925b6dd4068374c9f97df78e293f8
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2026-01-24 23:38:02 +0000

    Consolidate and fully test PathHelper

--- addons/sv_mod_loader/utility/path_helper.gd
+++ addons/sv_mod_loader/utility/path_helper.gd
@@ -12,3 +12,20 @@ static func filename_to_path(filename: String, dir: String) -> String:
 	var base_path: String = dir if dir.ends_with("/") else dir + "/"
 	
 	return base_path + filename
+
+
+## Given a path to a file, returns the path to that file's containing directory
+## (i.e. strips the filename and trailing backslash off the path)
+static func file_path_to_containing_dir(path: String) -> String:
+	var dir = path.rsplit("/", true, 1)[0]
+	
+	# Repair base dirs like user:// or res://, which may now be missing a forward-slash.
+	if dir.ends_with(":/"):
+		dir = dir + "/"
+	
+	return dir
+
+
+## Given a path to a file, returns only the filename
+static func file_path_to_filename(path: String) -> String:
+	return path.rsplit("/", true, 1)[1]
